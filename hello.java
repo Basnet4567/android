c610a690acdfa60a2d95c13470ed5941b576d349
suraj <surajee456@gmail.com>
2023-02-06 18:42:54 +0900

    test done

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class hello {
+    public static void main(String[] args) {
+        int a=2;
+        int hello =45637892;
+        System.out.println(a);
+        System.out.println(hello);
+
+    }
+
+}
+
